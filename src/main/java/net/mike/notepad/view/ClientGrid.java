6b0e6c9f14ce2f4ea57291e3685b4753d5d164db
sadosdesperados <bms39rus@gmail.com>
2020-10-04 18:44:52 +0200

    Debag

--- src/main/java/net/mike/notepad/view/ClientGrid.java
+++ src/main/java/net/mike/notepad/view/ClientGrid.java
@@ -12,35 +12,20 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 import net.mike.notepad.controller.services.NotesService;
-import net.mike.notepad.model.etyties.Account;
 import net.mike.notepad.model.etyties.Note;
-import net.mike.notepad.model.etyties.UserProfile;
-import org.apache.openjpa.lib.util.Services;
-import java.util.List;
 
 @Route("grid")
 @Theme(variant = Lumo.DARK, value = Lumo.class )
-public class ClientNotes extends VerticalLayout {
+public class ClientGrid extends VerticalLayout {
     private TextField textArea;
     private Grid<Note> wrapper;
     private NotesService request;
-    private List<Note> notesList;
-    private Account account;
 
-    public ClientNotes() {
-        UserProfile userProfile = new UserProfile(1, "aaa", "bbb");
-        Account account = new Account(userProfile.getId(), userProfile);
-        Note note = new Note(1,"first note", "this is textarea first note");
-        Note note1 = new Note(2,"first note", "this is textarea first note");
-        Note note2 = new Note(3,"first note", "this is textarea first note");
-        Note note3 = new Note(4,"first note", "this is textarea first note");
-        NotesService notesService = new NotesService(account);
-        notesService.saveNote(note);
-        notesService.saveNote(note1);
-        notesService.saveNote(note2);
-        notesService.saveNote(note3);
+
+    public ClientGrid() {
+
         //request = new NotesService(account);
-       // wrapper = new Grid<>(Note.class);
+        // wrapper = new Grid<>(Note.class);
         textArea = new TextField();
         textArea.setPlaceholder("Enter new note");
         //textArea.addValueChangeListener(this::onNameFilterTextChange);
