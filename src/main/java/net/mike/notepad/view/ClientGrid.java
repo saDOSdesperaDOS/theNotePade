fe9a4968fcbcdf4fb616309a298fa52444924204
sadosdesperados <bms39rus@gmail.com>
2021-01-10 06:48:41 +0200

    refactoring

--- src/main/java/net/mike/notepad/view/ClientGrid.java
+++ src/main/java/net/mike/notepad/view/ClientGrid.java
@@ -1,6 +1,7 @@
 package net.mike.notepad.view;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.AbstractGridSingleSelectionModel;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -8,6 +9,8 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.data.selection.SelectionEvent;
+import com.vaadin.flow.data.selection.SelectionListener;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
@@ -27,6 +30,10 @@ public class ClientGrid extends HorizontalLayout {
 
     private Logger log = LoggerFactory.getLogger(ClientGrid.class);
 
+    private Note n = new Note( 12,"first test tittle", "textarea of first note");
+    private Note n1 = new Note( 13,"first tittle", "textarrea of first note");
+    private Note n2 = new Note(14,"second tittle", "textarrea of second note");
+
     public ClientGrid() {
 
         setMinHeight("100%");
@@ -38,36 +45,34 @@ public class ClientGrid extends HorizontalLayout {
         UserProfile userProfile = new UserProfile(123, "email", "password");
         Account account = new Account(123, userProfile);
         NotesService service = new NotesService(account);
-
-
-        Note n = new Note( 12,"first test tittle", "textarea of first note");
-        Note n1 = new Note( 13,"first tittle", "textarrea of first note");
-        Note n2 = new Note(14,"second tittle", "textarrea of second note");
+        Grid<Note> grid = new Grid<>();
+        Grid.Column<Note> tittleColumn = grid.addColumn(Note::getTittle);
+        Grid.Column<Note> dateColumn = grid.addColumn(Note::getDate);
+        TextArea  textArea = new TextArea();
+        TextArea  textFieldTittle = new TextArea();
+        Note selectedNote = new Note();
 
         service.saveNote(n);
         service.saveNote(n1);
         service.saveNote(n2);
 
-        Grid<Note> grid = new Grid<>();
         grid.setItems(service.getAccount().getNotesList());
-        Grid.Column<Note> tittleColumn = grid.addColumn(Note::getTittle);
-        Grid.Column<Note> dateColumn = grid.addColumn(Note::getDate);
-
-        TextArea  textArea = new TextArea();
-        TextArea  textFieldTittle = new TextArea();
-        Note selectedNote = new Note();
-
+        grid.asSingleSelect().addValueChangeListener(event -> {
+            String message = String.format("Selection changed from %s to %s",
+                    event.getOldValue().getId(), event.getValue().getId());
+            Notification.show(message);
+        });
         grid.addItemClickListener(event -> {
             textArea.setValue(event.getItem().getTextArea());
             textFieldTittle.setValue(event.getItem().getTittle());
             selectedNote.setId(event.getItem().getId());
         });
 
-        Button saveButton = new Button("Save",
-                event -> {
-                            service.updateNote(service.find(selectedNote.getId()), textFieldTittle.getValue(), textArea.getValue());
-                            grid.getDataProvider().refreshAll();
-        });
+        Button saveButton = new Button("Save", event -> {
+                    service.updateNote(service.find(selectedNote.getId()), textFieldTittle.getValue(), textArea.getValue());
+                    grid.getDataProvider().refreshAll();
+                }
+        );
 
         Button button = new Button("Create a new Note", event -> {
             Note note = new Note(114);
@@ -75,10 +80,7 @@ public class ClientGrid extends HorizontalLayout {
             textArea.setValue(note.getTextArea());
             textFieldTittle.setValue(note.getTittle());
             grid.getDataProvider().refreshAll();
-
         });
-
-        tittleColumn.setEditorComponent(textFieldTittle);
 
         grid.setWidth("100%");
         textArea.setWidth("100%");
@@ -91,7 +93,6 @@ public class ClientGrid extends HorizontalLayout {
         layoutVerticalLeft.setMaxHeight("100%");
         layoutVerticalRight.setMinHeight("100%");
         layoutVerticalRight.setMaxHeight("100%");
-
         layoutVerticalRight.add(saveButton,textFieldTittle, textArea);
         layoutVerticalLeft.add(grid, button);
         add(layoutVerticalLeft, layoutVerticalRight);
